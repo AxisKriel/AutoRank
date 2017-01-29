71a51b9a068a918cee63ca4a598abef2553ecb4c
Enerdy <rodrigo.rente@live.com>
2017-01-29 16:45:59 +0000

    API 2.0

--- AutoRank/AutoRank.cs
+++ AutoRank/AutoRank.cs
@@ -13,7 +13,7 @@ using Wolfje.Plugins.SEconomy.Journal;
 
 namespace AutoRank
 {
-	[ApiVersion(1, 25)]
+	[ApiVersion(2, 0)]
 	public class AutoRank : TerrariaPlugin
 	{
 		public static Config Config { get; set; }
